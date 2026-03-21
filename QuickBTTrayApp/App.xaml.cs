7a4f2695065ad008e315532c9cadbe621e92cfe9
Joel-Hjerten <joel@joelart.com>
2026-03-21 02:13:13 +0100

    app starts with menu (not logic)

--- QuickBTTrayApp/App.xaml.cs
+++ QuickBTTrayApp/App.xaml.cs
@@ -4,5 +4,23 @@ namespace QuickBTTrayApp
 {
     public partial class App : Application
     {
+        private Hardcodet.Wpf.TaskbarNotification.TaskbarIcon _trayIcon;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            _trayIcon = (Hardcodet.Wpf.TaskbarNotification.TaskbarIcon)FindResource("TrayIcon");
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            _trayIcon?.Dispose();
+            base.OnExit(e);
+        }
+
+        private void TrayMenu_Exit_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Shutdown();
+        }
     }
 }
