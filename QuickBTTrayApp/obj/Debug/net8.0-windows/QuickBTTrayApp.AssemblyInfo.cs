61b498f7f1bdce6ac2e316ed3b7574487220548c
Joel-Hjerten <joel@joelart.com>
2026-03-23 20:39:57 +0100

    added new app screenshot

--- QuickBTTrayApp/obj/Debug/net8.0-windows/QuickBTTrayApp.AssemblyInfo.cs
+++ QuickBTTrayApp/obj/Debug/net8.0-windows/QuickBTTrayApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuickBTTray")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7ec84097c109fcb46dc38b663a609eb52deac41")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0365883c756c4989cf658ee449f303c84e609546")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuickBTTray")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuickBTTray")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0")]
